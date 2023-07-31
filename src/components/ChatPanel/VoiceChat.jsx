d9a42ece8044e8810c18de1c3d3bfbe8d8feb980
HexaCode-web <marcomark5050@gmail.com>
2023-07-31 06:08:19 +0300

    s

--- src/components/ChatPanel/VoiceChat.jsx
+++ src/components/ChatPanel/VoiceChat.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import AgoraRTC from "agora-rtc-sdk-ng";
 import { useSelector } from "react-redux";
 import { UPDATEDOC } from "../../server";
@@ -8,7 +8,7 @@ const VoiceChat = ({ FetchedCall }) => {
   const user = useSelector((state) => ({ ...state.user })).user;
   const activeChat = useSelector((state) => ({ ...state.chat }));
   const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
-
+  const [localAudioTrack, setLocalAudioTrack] = useState(null);
   client.on("user-published", async (user, mediaType) => {
     await client.subscribe(user, mediaType);
 
@@ -22,27 +22,34 @@ const VoiceChat = ({ FetchedCall }) => {
   });
 
   const StartCall = async () => {
-    // join a channel
-    await client.join(
-      "f891c234043549558a9d24395c86e4d4",
-      activeChat.chatID,
-      null,
-      user.uid
-    );
-    await UPDATEDOC("chats", activeChat.chatID, {
-      voiceCall: {
-        appId: "f891c234043549558a9d24395c86e4d4",
-        channel: activeChat.chatID,
-        token: null,
-      },
-    });
-    // create local audio track
-    const localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+    try {
+      // join a channel
+      await client.join(
+        "f891c234043549558a9d24395c86e4d4",
+        activeChat.chatID,
+        null,
+        user.uid
+      );
+      await UPDATEDOC("chats", activeChat.chatID, {
+        voiceCall: {
+          appId: "f891c234043549558a9d24395c86e4d4",
+          channel: activeChat.chatID,
+          token: null,
+        },
+      });
+      // create local audio track
+      setLocalAudioTrack(await AgoraRTC.createMicrophoneAudioTrack());
+      console.log("Local audio track created:", localAudioTrack);
 
-    // publish local audio track
-    await client.publish([localAudioTrack]);
+      // publish local audio track
+      await client.publish([localAudioTrack]);
+    } catch (error) {
+      console.error("Error starting call:", error);
+    }
   };
+
   const leaveCall = async () => {
+    localAudioTrack.close();
     await client.leave();
     await UPDATEDOC("chats", activeChat.chatID, {
       voiceCall: {

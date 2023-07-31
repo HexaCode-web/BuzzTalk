efeadd2177b7a12d232db4fecb336701f6e4eb30
HexaCode-web <marcomark5050@gmail.com>
2023-07-31 06:55:21 +0300

    s

--- src/components/ChatPanel/VoiceChat.jsx
+++ src/components/ChatPanel/VoiceChat.jsx
@@ -1,56 +1,58 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AgoraRTC from "agora-rtc-sdk-ng";
 import { useSelector } from "react-redux";
 import { UPDATEDOC } from "../../server";
 import voiceChat from "../../assets/voiceChat.png";
+
 const VoiceChat = ({ FetchedCall }) => {
   const user = useSelector((state) => ({ ...state.user })).user;
   const activeChat = useSelector((state) => ({ ...state.chat }));
-  const client = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+  const [client, setClient] = useState(null);
   const [localAudioTrack, setLocalAudioTrack] = useState(null);
-  client.on("user-published", async (user, mediaType) => {
-    await client.subscribe(user, mediaType);
+  const [inCall, setInCall] = useState(false); // Track call status
 
-    // if the media type is audio
-    if (mediaType === "audio") {
-      // get the audio track
-      const remoteAudioTrack = user.audioTrack;
-      // play the audio
-      remoteAudioTrack.play();
-    }
-  });
+  useEffect(() => {
+    // Create Agora client when component mounts
+    const agoraClient = AgoraRTC.createClient({ mode: "rtc", codec: "vp8" });
+    setClient(agoraClient);
+  }, []);
 
   const StartCall = async () => {
-    try {
-      // join a channel
-      await client.join(
-        "f891c234043549558a9d24395c86e4d4",
-        activeChat.chatID,
-        null,
-        user.uid
-      );
-      await UPDATEDOC("chats", activeChat.chatID, {
-        voiceCall: {
-          appId: "f891c234043549558a9d24395c86e4d4",
-          channel: activeChat.chatID,
-          token: null,
-        },
-      });
-      // create local audio track
-      setLocalAudioTrack(await AgoraRTC.createMicrophoneAudioTrack());
-      console.log("Local audio track created:", localAudioTrack);
+    if (inCall) {
+      // If already in a call, leave the channel
+      await leaveCall();
+    } else {
+      // If not in a call, join the channel
+      try {
+        // join a channel
+        await client.join(
+          "f891c234043549558a9d24395c86e4d4",
+          activeChat.chatID,
+          null,
+          user.uid
+        );
+        await UPDATEDOC("chats", activeChat.chatID, {
+          voiceCall: {
+            appId: "f891c234043549558a9d24395c86e4d4",
+            channel: activeChat.chatID,
+            token: null,
+          },
+        });
+        // create local audio track
+        const localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+        setLocalAudioTrack(localAudioTrack);
+        console.log("Local audio track created:", localAudioTrack);
 
-      // publish local audio track
-      await client.publish([localAudioTrack]);
-    } catch (error) {
-      console.error("Error starting call:", error);
+        // publish local audio track
+        await client.publish([localAudioTrack]);
+        setInCall(true); // Set the call status to true
+      } catch (error) {
+        console.error("Error starting call:", error);
+      }
     }
   };
 
   const leaveCall = async () => {
-    await client.leave();
-    client.release();
-
     await UPDATEDOC("chats", activeChat.chatID, {
       voiceCall: {
         appId: "",
@@ -59,11 +61,13 @@ const VoiceChat = ({ FetchedCall }) => {
         uid: null,
       },
     });
+    await client?.leave(); // Ensure the client exists before calling leave()
+    setInCall(false); // Set the call status to false
   };
 
   return (
     <div style={{ display: "flex" }}>
-      <p onClick={leaveCall}>Leave</p>
+      <p onClick={leaveCall}>{inCall ? "Leave" : "End Call"}</p>
       <img src={voiceChat} onClick={StartCall} />
       <div
         id="circle"
